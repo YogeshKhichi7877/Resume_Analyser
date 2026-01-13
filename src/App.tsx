15f05c324a22f7aa3f35869b9b9a46f8ab6c5285
Yogesh <ykhinchi38@gmail.com>
2026-01-13 13:58:47 +0530

    new url

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const DarkModeContext = React.createContext({
 
 // let ApiUrl = 'http://localhost:3011'
 
-let ApiUrl = 'https://resume-analyser-mwlu.onrender.com'
+let ApiUrl = 'https://resume-analyser-ypan.onrender.com'
 
 // --- AUTH PAGE COMPONENT (Neobrutalist Style) ---
 const AuthPage = ({ onLoginSuccess }: { onLoginSuccess: (token: string, userData: any) => void }) => {
